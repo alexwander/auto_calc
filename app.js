e2050cb722000d54d1cafbfb39031e603ab827cb
alexwander <ryndyuk.alexander@gmail.com>
2016-09-17 22:24:15 +0300

    add custom filter

--- app.js
+++ app.js
@@ -7,6 +7,14 @@
         $scope.name = "test";
         $scope.totalValue = 0;
       });
+
     
+    function TruthFilter() {
+        return function (input, target, replace) {
+            input = input || "";
+            input = input.replace(target, replace);
+            return input;
+        }
+    }
 
 })();
